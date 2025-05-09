6dbb299ce3ea23f0ebdc75821c3dcc743a37e4f9
anmol-jha21 <anmoljha2106@gmail.com>
2025-05-09 17:05:54 +0530

    git push in test branch

--- src/main/java/com/learning/quizapp_monolith/QuizappMonolithApplication.java
+++ src/main/java/com/learning/quizapp_monolith/QuizappMonolithApplication.java
@@ -9,6 +9,6 @@ public class QuizappMonolithApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuizappMonolithApplication.class, args);
 	}
-//test comment for git push
+//test comment for git push in test branch
 }
 
